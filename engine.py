ce1ba1ab24b37b15f85a781b1417b404368ac64f
sliitguy <it23259416@my.sliit.lk>
2025-07-07 10:37:15 +0530

    updating the code after correcting the errors

--- engine.py
+++ engine.py
@@ -1,73 +1,79 @@
 import cv2
 from ultralytics import YOLO
-import numpy as np 
-import torch 
+import numpy as np
+import torch
 
-class CustomerSegmentationWithYolo():
-    def __init__(self, erode_intensity=2, erode_size=5):
-        self.model = YOLO('yolov8m-seg.pt')
-        self.background_image=cv2.imread('./static/background2.jpg')
-        self.erode_size = erode_size
-        self.erode_intensity=erode_intensity
-
-    def generate_mask_from_result(self, results):
+class CustomSegmentationWithYolo():
+    def __init__(self, erode_size=5, erode_intensity=2):
+         self.model = YOLO('yolov8m-seg.pt')
+         self.erode_size = erode_size
+         self.erode_intensity = erode_intensity
+         self.background_image = cv2.imread("./static/background2.jpg")
+         
+    def generate_mask_from_result(self,results):
         for result in results:
             if result.masks:
-                # Get teh array results
-                masks=result.masks.data
-                boxes=result.boxes.data
-
-                # Extract classes 
+                # get array results
+                masks = result.masks.data
+                boxes = result.boxes.data
+                
+                # extract classes
                 clss = boxes[:, 5]
-
-                # Get the incides of results where class is 0 (people in COCO)
-                people_indices = torch.where(clss==0)
-
-                ## Use these indices to extract the relevant masks
+                
+                # get indices of results where class is 0 (people in COCO)
+                people_indices = torch.where(clss == 0)
+                
+                # use these indices to extract the relevant masks
                 people_masks = masks[people_indices]
 
                 if len(people_masks) == 0:
-                    return None
+                        return None
                 
-                ## Scale for visualizing results by getting the original frame
+                # scale for visualizing results
                 people_mask = torch.any(people_masks, dim=0).to(torch.uint8) * 255
 
-                ## To improve the edges of prediction
-                kernel=np.ones((self.erode_size, self.erode_size), np.uint8)
+                # Erode the mask to bring boundaries inward
+                '''
+                Erosion scans the image with a small kernel (a tiny matrix) and replaces the pixel value with 
+                the minimum value under the kernel. So if even one pixel under the kernel is black, the pixel 
+                being processed will turn black. This has the effect of eating away the white areas, shrinking the mask.
+                '''
+                # iterations - number of times to apply the erosion
+                # kernel - This is a small matrix (typically a square) that slides over the image.
+                kernel = np.ones((self.erode_size, self.erode_size), np.uint8)
                 eroded_mask = cv2.erode(people_mask.cpu().numpy(), kernel, iterations=self.erode_intensity)
 
+                # save to file
                 return eroded_mask
             else:
-                return None
-            
-    def apply_blur_with_mask(self, frame, mask, blur_strength=21):
+                 return None
+
+
+    def apply_blur_with_mask(self,frame, mask, blur_strength=21):
         blur_strength=(blur_strength, blur_strength)
-        blurred_frame=cv2.GaussianBlur(frame, blur_strength, 0)
-
-        # Ensure that the mask is binary
+        blurred_frame = cv2.GaussianBlur(frame, blur_strength, 0)
+        
+        # Ensure mask is binary
         mask = (mask > 0).astype(np.uint8)
-
+        
         # Expand mask to 3 channels
         mask_3d = cv2.merge([mask, mask, mask])
-
-        # combine blurred and original frame
+        
+        # Combine blurred and original frame
         result_frame = np.where(mask_3d == 1, frame, blurred_frame)
-
+        
         return result_frame
     
     def apply_black_background(self, frame, mask):
         # Create a black background
-        black_background=np.zero_like(frame)
-
+        black_background = np.zeros_like(frame)
         # Apply the mask
-        result_frame=np.where(mask[:, :, np.newaxis] == 255, frame, black_background)
+        result_frame = np.where(mask[:, :, np.newaxis] == 255, frame, black_background)
         return result_frame
 
     def apply_custom_background(self, frame, mask):
         # Load the background image
-        background_image=cv2.resize(self.background_image, (frame.shape[1], frame.shape[0]))
-
+        background_image = cv2.resize(self.background_image, (frame.shape[1], frame.shape[0]))
         # Apply the mask
-        result_frame=np.where(mask[:, :, np.newaxis] == 255, frame, background_image)
+        result_frame = np.where(mask[:, :, np.newaxis] == 255, frame, background_image)
         return result_frame
-
--- main.py
+++ main.py
@@ -1,57 +1,63 @@
 import uvicorn
+import numpy as np
 from fastapi import FastAPI, Query
 from fastapi.responses import JSONResponse, FileResponse
 from fastapi.staticfiles import StaticFiles
-from stream_utils import Streaming
 import threading
+from stream_utils import Streaming
 
-app=FastAPI()
+app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-streaming=Streaming()
+stream_thread = None
 
-stream_thread=None
+streaming = Streaming()
+
+# Set below based on your webcam fps, by default is 15.
+# streaming.update_cam_fps(15)
+
 
 @app.get("/")
 def serve_ui():
     return FileResponse("static/index.html")
 
-@app.get('/start')
-def start_streaming(
-    source: str= Query("0"),
-    fps: int= Query(15),
+
+@app.get("/start")
+def start_stream(
+    source: str = Query("0"),
+    fps: int = Query(15),
     blur_strength: int = Query(21),
     background: str = Query("none")
 ):
     streaming.update_streaming_config(in_source=source, out_source=None, fps=fps, blur_strength=blur_strength, background=background)
-    
+
+    global stream_thread
+
     if streaming.running:
         return JSONResponse(content={"message": "Stream already running"}, status_code=400)
 
-    stream_thread=threading.Thread(
+    if fps < 1 or fps > 60:
+        return JSONResponse(content={"message": "Invalid FPS value (1-60)"}, status_code=400)
+
+    stream_thread = threading.Thread(
         target=streaming.stream_video, args=()
     )
-
     stream_thread.start()
+    return {"message": f"Streaming started from source: {fps} FPS and blur strength {blur_strength}"}
 
-    return {"message": f"Streaming started from source: {fps} FPS and blur strength {blur_strength}."}
-    
+
 @app.get("/stop")
 def stop_stream():
-    """
-    Stops the stream
-    """
-    return streaming.update_running_status() 
+    streaming.update_running_status()
+    return {"message": "Streaming stopped"}
+
 
 @app.get("/devices")
 def devices():
-    """
-    Returns the number of webcams we can access in the system
-    """
-    return streaming.list_available_devices() 
-    
+    return streaming.list_available_devices()
+
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
 
+    uvicorn.run(app, host="0.0.0.0", port=8000)
